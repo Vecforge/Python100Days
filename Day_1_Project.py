b66cf7ea17148516500ad0a0ded89c7ff45fa64d
Vecforge <devansh.ps002@gmail.com>
2024-07-26 22:24:09 +0530

    Day 1 Project

---
+++ Day_1_Project.py
@@ -0,0 +1,7 @@
+#------------------------Band Name Generator----------------------------
+print("Welcome to the Band Name Generator")
+print("What's the name of the city you grew up in?")
+c_name = input()
+print("What's your pet's name?")
+p_name = input()
+print("Your band name could be " + c_name + " " + p_name)
